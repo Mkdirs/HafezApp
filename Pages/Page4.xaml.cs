f3877b58fa4bac86032b2035da5f6a8712efeb84
Hafez Ben-Abdillah MADI <hafezbenabdillah.madi@gmail.com>
2024-04-07 13:41:19 +0200

    ajout cookie clicker

--- Pages/Page4.xaml.cs
+++ Pages/Page4.xaml.cs
@@ -2,8 +2,23 @@ namespace HafezApp.Pages;
 
 public partial class Page4 : ContentPage
 {
+	int clicks = 0;
 	public Page4()
 	{
 		InitializeComponent();
 	}
+
+	private async void OnCookieClick(object sender, EventArgs e)
+	{
+		var image = sender as ImageButton;
+		image.WidthRequest += 10;
+		image.HeightRequest += 10;
+		await Task.Delay(100);
+        image.WidthRequest -= 10;
+        image.HeightRequest -= 10;
+
+		clicks++;
+
+		label.Text = $"{clicks}";
+    }
 }
